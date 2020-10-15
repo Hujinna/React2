171b003c56723589665376bec9bdd08aa02e4765
Hjinna <1326687396@qq.com>
2020-10-15 19:18:29 +0800

    react2

---
+++ precache-manifest.d76644b5e126fbb7ee46eabf34b37a2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a28a98d985e81ad14e70bd463b0624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a649f9c3bacfdce0542a",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "42d4b95eec2b0a3258a3",
+    "url": "/static/css/main.2d5b6c6c.chunk.css"
+  },
+  {
+    "revision": "a649f9c3bacfdce0542a",
+    "url": "/static/js/2.0c199b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c199b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42d4b95eec2b0a3258a3",
+    "url": "/static/js/main.0ba9628f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
